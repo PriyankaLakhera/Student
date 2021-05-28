90ba5d0592f5d011b0e7fbbd9c967719218e3ba5
pc <pc@dell>
2021-05-28 21:44:39 +0530

    demo commit

---
+++ src/week1/Student.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week1;
+
+/**
+ *
+ * @author pc
+ */
+public class Student {
+    private String name;
+    private String studentID;
+
+    public Student(String givenName) 
+    {
+        name = givenName;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public void setName(String givenName) 
+    {
+        name = givenName;
+    }
+
+    public String getStudentID() 
+    {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) 
+    {
+        this.studentID = studentID;
+    }
+    
+}
